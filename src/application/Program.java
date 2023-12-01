df778f97691f698fde8911e00b38a99149ddb782
Luiz Covre <luiz.1covre@gmail.com>
2023-11-30 22:07:05 -0300

    Inicio do classe Posição

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olá otários!!!");
+
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
