c04243ab869864b39ee49a5ef24f8db26289f865
Luiz Covre <luiz.1covre@gmail.com>
2023-12-04 19:44:33 -0300

    melhorias no tabuleiro e cores

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -7,29 +7,39 @@ import xadrez.pecas.Torre;
 public class PartidaXadrez {
 
 	private Tabuleiro tabuleiro;
-	
+
 	public PartidaXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
 		setupInicial();
 	}
-	
+
 	public PecaXadrez[][] getPecas() {
 		PecaXadrez[][] mat = new PecaXadrez[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-		for (int i=0; i<tabuleiro.getLinhas(); i++) {
-			for (int j=0; j<tabuleiro.getColunas(); j++) {
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
 				mat[i][j] = (PecaXadrez) tabuleiro.peca(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	private void colocaNovaPeca(char coluna, int linha, PecaXadrez peca) {
 		tabuleiro.colocaPeca(peca, new PosicaoXadrez(coluna, linha).toPosition());
 	}
-	
+
 	private void setupInicial() {
-		colocaNovaPeca('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-		colocaNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
-		colocaNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		colocaNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+		colocaNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+		colocaNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+		colocaNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+		colocaNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+		colocaNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+		colocaNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
+		colocaNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
+		colocaNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
+		colocaNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
+		colocaNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
+		colocaNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
 	}
 }
